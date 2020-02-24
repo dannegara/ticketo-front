dc1a037e4fca0b382774d3713f9042fafa19f50d
Dan Negara <dannegareh@gmail.com>
2020-02-24 19:17:18 +0200

    sidebar created

--- src/components/UI/navbar/component.tsx
+++ src/components/UI/navbar/component.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
 import Button from '../button';
 import Logo from '../logo';
 
-interface INavBarProps{
-    name?: string
-}
+class NavBar extends Component {
 
-const NavBar : React.SFC<INavBarProps> = ( ) => {
-    return(
-        <nav>
-            <div className="logo-container">
-                <Link to="/"><Logo /></Link>
+    state = {
+        sidebar: ["sidebar"],
+        background: ["sidebar-background"]
+    }
+
+    turnSidebar = () => {
+        this.setState({
+            sidebar: ["sidebar", "show-sidebar"],
+            background: ["sidebar-background", "show-sidebar-background"]
+        });
+    }
+
+    removeSidebar = () => {
+        this.setState({
+            sidebar: ["sidebar"],
+            background: ["sidebar-background"]
+        });
+    }
+
+    render(){
+        const { sidebar, background } = this.state;
+        return(
+            <div>
+                <nav>
+                    <div className="side-menu-opener" onClick={this.turnSidebar}>
+                        <span className="sandwich"></span>
+                        <span className="sandwich"></span>
+                        <span className="sandwich"></span>
+                    </div>
+                    <div className="logo-container">
+                        <Link to="/"><Logo /></Link>
+                    </div>
+                    <div className="links-container">
+                        <Link to="/login"><Button>Login</Button></Link>
+                        <Link to="/register"><Button>Register</Button></Link>
+                    </div>
+                </nav>
+                <div className="sidebar-container">
+                    <div className={background.join(' ')} onClick={this.removeSidebar}></div>
+                    <div className={sidebar.join(' ')}>
+                        <ul>
+                            <li>fhdsbkasbs</li>
+                            <li>fhdsbkasbs</li>
+                            <li>fhdsbkasbs</li>
+                            <li>fhdsbkasbs</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div className="links-container">
-                <Link to="/login"><Button>Login</Button></Link>
-                <Link to="/register"><Button>Register</Button></Link>
-            </div>
-        </nav>
-    )
+        )
+    }
 }
 
 export default NavBar;
